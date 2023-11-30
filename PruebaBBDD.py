be78abb2765b2132303a79894486b673fd31e6ec
informaticosonline <109153512+informaticosonline@users.noreply.github.com>
2023-11-30 19:35:22 +0100

    Script para prueba de conexión

---
+++ PruebaBBDD.py
@@ -0,0 +1,42 @@
+"""
+Curso Python empresa
+
+Autor: José Antonio Calvo López
+Fecha: Noviembre de 2023
+"""
+
+import mysql.connector
+from mysql.connector import Error
+
+HOST = "178.211.133.56"
+USER = "ofluqcym_cursoFor"
+PASSWORD = "Studium2020;"
+DATABASE = "ofluqcym_CursoPy"
+
+def conectar():
+    return mysql.connector.connect(
+        host=HOST,
+        user=USER,
+        password=PASSWORD,
+        database=DATABASE
+    )
+
+def probar_conexion():
+    try:
+        conexion = conectar()
+        if conexion.is_connected():
+            db_Info = conexion.get_server_info()
+            print("Conectado a MySQL Server versión ", db_Info)
+            cursor = conexion.cursor()
+            cursor.execute("select database();")
+            record = cursor.fetchone()
+            print("Conectado a la base de datos: ", record)
+            
+        cursor.close()
+        conexion.close()
+        print("La conexión a MySQL se ha cerrado")
+
+    except Error as e:
+        print("Error al conectar a MySQL", e)
+
+probar_conexion()
